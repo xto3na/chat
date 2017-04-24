ca5073cdbc2756c691c0f64a854c3562dd06e2f2
Sergey Ponomarenko <xto3na@gmail.com>
2017-04-25 01:06:29 +0300

    Add components.

---
+++ Chat/Handlers/RouteHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Chat.Handlers
+{
+    public class RouteHandler : IRouteHandler
+    {
+      public IHttpHandler GetHttpHandler(RequestContext requestContext)
+      {
+        requestContext.HttpContext.Response.Redirect("#" + requestContext.HttpContext.Request.FilePath);
+        return new MyHttpHandler();
+      }
+    }
+
+    public class MyHttpHandler : IHttpHandler
+    {
+      public bool IsReusable
+      {
+        get { return false; }
+      }
+
+      public void ProcessRequest(HttpContext context)
+      {
+        context.Response.Write("Инопланетное послание");
+      }
+    }
+}
--- Chat/src/app/app.module.ts
+++ Chat/src/app/app.module.ts
@@ -2,19 +2,54 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { ChatComponent } from './chat/chat.component';
+
+const appRoutes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'chat', component: ChatComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'signup', component: SignupComponent },
+	// {
+	// 	path: 'words',
+	// 	component: WordsComponent
+   //  // ,children: [
+   //  //   { path: '', component: AboutHomeComponent }, // url: about/
+   //  //   { path: 'item/:id', component: AboutItemComponent } // url: about/item
+   //  // ]
+	// },
+	// { path: 'contact', component: ContactComponent },
+	// { path: 'about', component: AboutComponent },
+	{ path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NotFoundComponent,
+		HomeComponent,
+		LoginComponent,
+		MenuComponent,
+		SignupComponent,
+		ChatComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		RouterModule.forRoot(appRoutes),
+		HttpModule
+	],
+	providers: [
+		{provide: LocationStrategy, useClass: HashLocationStrategy}
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
